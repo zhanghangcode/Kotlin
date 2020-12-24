cd77b2dd386624c5de265907b1f62d474aad1325
zhanghang <17719916361@163.com>
2020-12-24 22:27:49 +0900

    java言語→kotlin言語に転換練習 add Utils.kt

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.app.entity.User
 import com.example.app.widget.CodeView
 import com.example.core.utils.CacheUtils
-import com.example.core.utils.Utils
+import com.example.core.utils.toast
 import com.example.lesson.LessonActivity
 
 class MainActivity:AppCompatActivity(), View.OnClickListener {
@@ -61,11 +61,11 @@ class MainActivity:AppCompatActivity(), View.OnClickListener {
 
     private fun verify(user: User):Boolean{
         if (user.username!=null && user.username!!.length >4){
-            Utils.toast("用户名不合法")
+            toast("用户名不合法")
             return false
         }
         if (user.password!=null && user.password!!.length >4){
-            Utils.toast("密码不合法")
+            toast("密码不合法")
             return false
         }
         return true
--- app/src/main/java/com/example/app/widget/CodeView.kt
+++ app/src/main/java/com/example/app/widget/CodeView.kt
@@ -9,7 +9,7 @@ import android.util.TypedValue
 import android.view.Gravity
 import androidx.appcompat.widget.AppCompatTextView
 import com.example.app.R
-import com.example.core.utils.Utils
+import com.example.core.utils.dp2px
 import java.util.*
 
 class CodeView: AppCompatTextView {
@@ -33,7 +33,7 @@ class CodeView: AppCompatTextView {
         paint.isAntiAlias = true
         paint.style = Paint.Style.STROKE
         paint.color = getContext().getColor(R.color.colorAccent)
-        paint.strokeWidth = Utils.dp2px(6f)
+        paint.strokeWidth = dp2px(6f)
 
         updateCode()
     }
