d2c1e6406f7941fadeb41b5defc5ca85e0b0c166
zhanghang <17719916361@163.com>
2020-12-29 16:45:13 +0900

    DashBoard

---
+++ app/src/main/java/com/example/app/MyUiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyUiActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_myui)
+    }
+}
---
+++ app/src/main/java/com/example/app/ui/DashboardView.kt
@@ -0,0 +1,64 @@
+package com.example.app.ui
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import java.util.jar.Attributes
+import javax.xml.namespace.NamespaceContext
+import kotlin.math.cos
+import kotlin.math.sin
+
+val RADIUS=150f.px
+const val OPEN_ANGLE=120f
+val LENGTH=120f.px
+val DASH_WIDTH=2f.px
+val DASH_LENGTH=10f.px
+class DashboardView(context: Context?,attrs: AttributeSet?):
+        View(context,attrs){
+
+    private val dash=Path()
+    private val path=Path()
+    private lateinit var pathEffect: PathEffect
+    private val paint=Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            strokeWidth=3f.px
+            style=Paint.Style.STROKE
+
+
+    }
+
+
+    init {
+        dash.addRect(0f,0f, DASH_WIDTH, DASH_LENGTH,Path.Direction.CCW)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        path.reset()
+        path.addArc(width/2f-RADIUS,height/2f-RADIUS,width/2f+RADIUS,height/2f+RADIUS,
+                90+ OPEN_ANGLE/2f,360- OPEN_ANGLE)
+        val pathMeasure=PathMeasure(path,false)
+        pathEffect=PathDashPathEffect(dash,(pathMeasure.length- DASH_WIDTH)/20f,0f,PathDashPathEffect.Style.ROTATE)
+
+    }
+
+
+    override fun onDraw(canvas: Canvas) {
+        //1 画弧
+        canvas.drawPath(path,paint)
+        //2画刻度
+        paint.pathEffect=pathEffect
+        canvas.drawPath(path,paint)
+        paint.pathEffect=null
+
+        canvas.drawLine(width/2f,height/2f,
+                (width/2f+ LENGTH* cos(markToRadians(10))).toFloat(),
+                (height/2f+ LENGTH* sin(markToRadians(10))).toFloat(),
+                paint
+        )
+
+    }
+
+    private fun markToRadians(mark:Int)= Math.toRadians((90+ OPEN_ANGLE/2f+(360- OPEN_ANGLE)/20f*mark).toDouble())
+
+
+}
