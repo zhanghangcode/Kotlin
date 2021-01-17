46bae97da5c1b72272daa4301b684f8454c8f492
zhanghang <17719916361@163.com>
2021-01-17 14:45:35 +0900

    アニメーション

--- app/src/main/java/com/example/app/MyUiActivity.kt
+++ app/src/main/java/com/example/app/MyUiActivity.kt
@@ -1,5 +1,9 @@
 package com.example.app
 
+import android.animation.AnimatorSet
+import android.animation.Keyframe
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
@@ -8,6 +12,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.app.threadrxjava.Api
 import com.example.app.threadrxjava.model.Repo
+import com.example.app.ui.dp
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Scheduler
@@ -17,6 +22,7 @@ import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.functions.Function
 import io.reactivex.rxjava3.plugins.RxJavaPlugins
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_myui.*
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -32,6 +38,44 @@ class MyUiActivity:AppCompatActivity() {
         setContentView(R.layout.activity_myui)
         drawBadge()
 
+
+//        val bottomFlipAnimator=ObjectAnimator.ofFloat(view,"bottomFilp",60f)
+//        bottomFlipAnimator.startDelay=1000
+//        bottomFlipAnimator.duration=1000
+
+//        val flipFlipAnimator=ObjectAnimator.ofFloat(view,"flipRotation",270f)
+//        flipFlipAnimator.startDelay=200
+//        flipFlipAnimator.duration=1000
+//
+//        val topFlipAnimator=ObjectAnimator.ofFloat(view,"topFlip",-60f)
+//        topFlipAnimator.startDelay=200
+//        topFlipAnimator.duration=1000
+//
+//        val animatorSet=AnimatorSet()
+//        animatorSet.playSequentially(bottomFlipAnimator,flipFlipAnimator,topFlipAnimator)
+//        animatorSet.start()
+//
+//        val bottomFlipAHolder=PropertyValuesHolder.ofFloat("bottomFilp",60f)
+//        val flipFlipAHolder=PropertyValuesHolder.ofFloat("flipRotation",270f)
+//        val topFlipAHolder=PropertyValuesHolder.ofFloat("topFlip",-60f)
+//
+//        val holderAnimator=ObjectAnimator.ofPropertyValuesHolder(view,bottomFlipAHolder,flipFlipAHolder,topFlipAHolder)
+//        holderAnimator.startDelay=1000
+//        holderAnimator.duration=2000
+//        holderAnimator.start()
+
+        val length=200.dp
+        val keyframe1=Keyframe.ofFloat(0f,0f)
+        val keyframe2=Keyframe.ofFloat(1.2f,0.4f*length)
+        val keyframe3=Keyframe.ofFloat(0.8f,0.6f*length)
+        val keyframe4=Keyframe.ofFloat(1f,1f*length)
+
+        val keyframeHolder=PropertyValuesHolder.ofKeyframe("translationX",keyframe1,keyframe2,keyframe3,keyframe4)
+
+        val animator=ObjectAnimator.ofPropertyValuesHolder(view,keyframeHolder)
+        animator.startDelay=1000
+        animator.duration=2000
+        animator.start()
     }
 
 
---
+++ app/src/main/java/com/example/app/ui/CameraAnView.kt
@@ -0,0 +1,91 @@
+package com.example.app.ui
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import com.example.app.R
+
+/**
+ * @Date 執筆時間 2021/01/10 14:49
+ * @Author Caden
+ * @Description
+ * @Email code_legend@163.com
+ * @Vesion 1.0
+ */
+
+private val BITMAP_SIZE=200.dp
+private val BITMAP_PADDING=100.dp
+class CameraAnView(context: Context,attrs:AttributeSet):View(context,attrs) {
+
+    private val paint=Paint(Paint.ANTI_ALIAS_FLAG)
+    private val bitmap=getAvatar(BITMAP_SIZE.toInt())
+    private val camera=Camera()
+
+    var topFlip=0f
+    set(value) {
+        field=value
+        invalidate()
+    }
+    var bottomFilp=0f
+        set(value) {
+            field=value
+            invalidate()
+        }
+    var flipRotation=0f
+        set(value) {
+            field=value
+            invalidate()
+        }
+
+
+    init {
+        //例外zはdpではなくて、insです。
+        camera.setLocation(0f,0f,-6 * resources.displayMetrics.density)
+    }
+    override fun onDraw(canvas: Canvas) {
+
+      //  canvas.withSave {}
+        //上半部分
+        canvas.save()
+        canvas.translate(BITMAP_PADDING+ BITMAP_SIZE/2,BITMAP_PADDING+ BITMAP_SIZE/2)
+        canvas.rotate(-flipRotation)
+        //画像折
+        canvas.save()
+        camera.rotateX(topFlip)
+        camera.applyToCanvas(canvas)
+        canvas.restore()
+
+        canvas.clipRect(-BITMAP_SIZE, -BITMAP_SIZE,BITMAP_SIZE,0f)
+        canvas.rotate(flipRotation)
+        canvas.translate(-(BITMAP_PADDING+ BITMAP_SIZE/2),-(BITMAP_PADDING+ BITMAP_SIZE/2))
+        canvas.drawBitmap(bitmap, BITMAP_PADDING, BITMAP_PADDING,paint)
+        canvas.restore()
+
+        //下半部分
+        canvas.save()
+        canvas.translate(BITMAP_PADDING+ BITMAP_SIZE/2,BITMAP_PADDING+ BITMAP_SIZE/2)
+        canvas.rotate(-flipRotation)
+        //画像折
+        canvas.save()
+        camera.rotateX(bottomFilp)
+        camera.applyToCanvas(canvas)
+        canvas.restore()
+
+        canvas.clipRect(-BITMAP_SIZE, 0f,BITMAP_SIZE, BITMAP_SIZE)
+        canvas.rotate(flipRotation)
+        canvas.translate(-(BITMAP_PADDING+ BITMAP_SIZE/2),-(BITMAP_PADDING+ BITMAP_SIZE/2))
+        canvas.drawBitmap(bitmap, BITMAP_PADDING, BITMAP_PADDING,paint)
+        canvas.restore()
+
+    }
+    private fun getAvatar(width:Int):Bitmap{
+       val options=BitmapFactory.Options()
+       options.inJustDecodeBounds=true
+       BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian,options)
+       options.inJustDecodeBounds=false
+        options.inDensity=options.outWidth
+        options.inTargetDensity=width
+        return BitmapFactory.decodeResource(resources,R.drawable.avatar_rengwuxian,options)
+    }
+}
